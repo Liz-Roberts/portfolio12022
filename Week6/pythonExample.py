f0aa736b62fd78f53cb215bc79772ecf85025cb2
Liz-Roberts <erober12@our.ecu.edu.au>
2022-02-27 08:37:25 +0000

    add Week6 to portfolio12022

---
+++ Week6/pythonExample.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+print ("Hi there!")
+
+value = "blah"
+
+y = 20
+
+print ("The value is %s" % value)
+if ( y < 10 ):
+
+    print( "Y is very small")
+else:
+    print("Y is very big")
+
+    print("This is some more text")
+
---
+++ Week6/rubyExamples.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/ruby
+
+puts "hello"
+puts ("Hi there")
+
